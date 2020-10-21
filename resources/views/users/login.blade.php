284d3d598df6885dc003c2e3dc831b8c801814a9
secwalker <1270291909@qq.com>
2020-10-21 11:07:53 +0800

    中文的错误提示信息

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.default')
+
 @section('content')
     <div class="login-logo">
         <h3>HBScanner</h3>
@@ -12,7 +13,7 @@
 
             <div class="widget-content" style="padding-bottom:0;">
                 <form method="POST" action="{{ route('users.store') }}" class="no-margin">
-                    <h3 class="form-title">用户注册</h3>
+                    <h3 class="form-title">用户登录</h3>
                     {{ csrf_field() }}
 
                     <fieldset>
@@ -24,18 +25,6 @@
                                     <i class="icon-user"></i>
                                 </span>
                                 <input type="text" name="name" placeholder="你的名称" value="{{ old('name') }}" class="form-control input-lg">
-                            </div>
-
-                        </div>
-
-                        <div class="form-group no-margin">
-                            <label for="email">邮箱：</label>
-
-                            <div class="input-group input-group-lg">
-                                <span class="input-group-addon">
-                                    <i class="icon-user"></i>
-                                </span>
-                                <input type="email" name="email" placeholder="你的邮箱" value="{{ old('email') }}" class="form-control input-lg">
                             </div>
 
                         </div>
@@ -52,21 +41,10 @@
 
                         </div>
 
-                        <div class="form-group">
-                            <label for="password">确认密码：</label>
-
-                            <div class="input-group input-group-lg">
-                                <span class="input-group-addon">
-                                    <i class="icon-lock"></i>
-                                </span>
-                                <input type="password" name="password_confirmation" placeholder="再次确认一下密码" value="{{ old('password_confirmation') }}" class="form-control input-lg">
-                            </div>
-
-                        </div>
                     </fieldset>
                     <div class="form-actions">
                         <button class="btn btn-warning pull-right" type="submit">
-                            注册 <i class="m-icon-swapright m-icon-white"></i>
+                            登录 <i class="m-icon-swapright m-icon-white"></i>
                         </button>
                         <div class="forgot"></div>
                     </div>
